e8a97bffce2ee0e124fcddf1f5956e3aeb8df964
g00229111 <raisingsun010@hotmail.com>
2017-09-22 17:09:41 -0400

    Mission Demolition

---
+++ Assets/FollowCam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public FollowCam S;
+
+    public bool _____________;
+
+    public GameObject poi;
+    public float camZ;
+
+    private void Awake()
+    {
+        S = this;
+        camZ = this.transform.position.z;
+    }
+
+
+
+
+}
