aec44a61fecb4af0235ca73f6c987f0880eff12b
g00229111 <raisingsun010@hotmail.com>
2017-09-26 19:11:55 -0400

    9/26/2017

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public FollowCam S;
+
+    public float easing = 0.05f;
+    public Vector2 minXY;
+    public bool _____________;
+
+    public GameObject poi;
+    public float camZ;
+
+    private void Awake()
+    {
+        S = this;
+        camZ = this.transform.position.z;
+    }
+
+
+
+    private void Update()
+    {
+        if (poi == null) return;
+        Vector3 destination = poi.transform.position;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        destination.z = camZ;
+        transform.position = destination;
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 destination;
+        if (poi == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            destination = poi.transform.position;
+
+            if (poi.tag == "Projectile")
+            {
+                if (poi.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    poi = null;
+                    return;
+                }
+
+            }
+        }
+    }
+}
